ce3a091ba1cb9351a505c7501f120a9b04b0bd0f
Cyber9ja <ayodejioladapo96@gmail.com>
2023-05-24 21:37:44 +0100

    update

--- vanilla-typescript/build/js/class.js
+++ vanilla-typescript/build/js/class.js
@@ -1,9 +1,27 @@
 "use strict";
-class Coder {
-    constructor(name, music, age, lang) {
-        this.name = name;
-        this.music = music;
-        this.name = name;
-        this.age = age;
+class Point {
+    constructor() {
+        this.x = 0;
+        this.y = 0;
     }
 }
+const point = new Point();
+console.log(point.x);
+// class Coder {
+//   // name: string;
+//   // music: string;
+//   // age: number;
+//   // lang: string;
+//   constructor(public readonly name: string, public music: string, private age: number, protected lang: string = 'Typescript') {
+//     this.name = name;
+//     this.music = music;
+//     this.lang = lang;
+//     this.age = 25;
+//   }
+//   public getAge(){
+//     return `Hello, I'm${this.age}`
+//   }
+// }
+// const Dave = new Coder('Dave', 'Rock', 23);
+// console.log(Dave.getAge());
+// // console.log(Dave.age);// console.log(Dave.lang);
--- vanilla-typescript/src/class.ts
+++ vanilla-typescript/src/class.ts
@@ -1,23 +1,32 @@
-class Coder {
-  // name: string;
-  // music: string;
-  // age: number;
-  // lang: string;
-  
-  constructor(public readonly name: string, public music: string, private age: number, protected lang: string = 'Typescript') {
-    this.name = name;
-    this.music = music;
-    this.lang = lang;
-    this.age = 25;
-  }
-
-
-  public getAge(){
-    return `Hello, I'm${this.age}`
-  }
+class Point{
+  x= 0;
+  y=0;
 }
 
+const point = new Point();
 
-const Dave = new Coder('Dave', 'Rock', 23);
-console.log(Dave.getAge());
-// console.log(Dave.age);// console.log(Dave.lang);
+console.log(point.x);
+
+// class Coder {
+//   // name: string;
+//   // music: string;
+//   // age: number;
+//   // lang: string;
+  
+//   constructor(public readonly name: string, public music: string, private age: number, protected lang: string = 'Typescript') {
+//     this.name = name;
+//     this.music = music;
+//     this.lang = lang;
+//     this.age = 25;
+//   }
+
+
+//   public getAge(){
+//     return `Hello, I'm${this.age}`
+//   }
+// }
+
+
+// const Dave = new Coder('Dave', 'Rock', 23);
+// console.log(Dave.getAge());
+// // console.log(Dave.age);// console.log(Dave.lang);
