050c667e22bc59bbfae6d709e8080fb8c27a08ad
Cyber9ja <ayodejioladapo96@gmail.com>
2023-05-25 10:21:03 +0100

    updated class

---
+++ vanilla-typescript/build/js/index_and_keyof.js
@@ -0,0 +1,18 @@
+"use strict";
+// Index Signatures
+const todaysTransaction = {
+    Pizza: -10,
+    Books: -5,
+    Job: 50,
+};
+console.log(todaysTransaction.Pizza);
+console.log(todaysTransaction['Pizza']);
+let prop = 'Pizza';
+console.log(todaysTransaction[prop]);
+const todaysNet = (transactions) => {
+    let total = 0;
+    for (const transaction in transactions) {
+        total += transactions[tranaction];
+    }
+    return total;
+};
---
+++ vanilla-typescript/src/index_and_keyof.ts
@@ -0,0 +1,27 @@
+// Index Signatures
+
+interface TransactionObj{
+  Pizza: number;
+  Books: number;
+  Job: number;
+}
+
+const todaysTransaction: TransactionObj = {
+  Pizza: -10,
+  Books: -5,
+  Job: 50,
+}
+
+console.log(todaysTransaction.Pizza);
+console.log(todaysTransaction['Pizza']);
+
+let prop: string = 'Pizza';
+console.log(todaysTransaction[prop]);
+
+const todaysNet = (transactions: TransactionObj): number => {
+  let total = 0;
+  for (const transaction in transactions) {
+    total += transactions[tranaction]
+  }
+  return total;
+}
