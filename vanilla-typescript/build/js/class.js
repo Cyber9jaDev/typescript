1d6825e7ae56a7e1d8dbff89ff54b6cdf859cc72
Cyber9ja <ayodejioladapo96@gmail.com>
2023-05-25 03:43:50 +0100

    minor update

--- vanilla-typescript/build/js/class.js
+++ vanilla-typescript/build/js/class.js
@@ -1,27 +1,78 @@
 "use strict";
-class Point {
-    constructor() {
-        this.x = 0;
-        this.y = 0;
-    }
-}
-const point = new Point();
-console.log(point.x);
-// class Coder {
-//   // name: string;
-//   // music: string;
-//   // age: number;
-//   // lang: string;
-//   constructor(public readonly name: string, public music: string, private age: number, protected lang: string = 'Typescript') {
-//     this.name = name;
-//     this.music = music;
-//     this.lang = lang;
-//     this.age = 25;
-//   }
-//   public getAge(){
-//     return `Hello, I'm${this.age}`
+// class Point{
+//   x : number;
+//   y : number;
+//   constructor(x: number, y:number){
+//     this.x = x;
+//     this.y = y;
 //   }
 // }
-// const Dave = new Coder('Dave', 'Rock', 23);
-// console.log(Dave.getAge());
-// // console.log(Dave.age);// console.log(Dave.lang);
+// const point = new Point(2,3);
+// point.x = 0;
+// point.y = 0;
+// console.log(point);
+class Coder {
+    // name: string;
+    // music: string;
+    // age: number;
+    // lang: string;
+    constructor(name, music, age, lang = 'Typescript') {
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+        this.name = name;
+        this.music = music;
+        this.lang = lang;
+        this.age = 25;
+    }
+    getAge() {
+        return `Hello, I'm ${this.age}`;
+    }
+}
+const Dave = new Coder('Dave', 'Rock', 23);
+console.log(Dave.getAge());
+// console.log(Dave.age);
+// console.log(Dave.lang)
+class WebDev extends Coder {
+    constructor(computer, name, music, age) {
+        super(name, music, age);
+        this.computer = computer;
+        this.computer = computer;
+    }
+    getLang() {
+        return `I write ${this.lang}`;
+    }
+}
+const Sara = new WebDev('Mac', 'Sara', 'Pop', 23);
+console.log(Sara.getLang());
+class Pianist {
+    constructor(name, instrument) {
+        this.name = name;
+        this.instrument = instrument;
+    }
+    play(action) {
+        return `${this.name} plays ${this.instrument} ${action}`;
+    }
+}
+const Orlando = new Pianist('Orlando Owo', 'Piano');
+console.log(Orlando.play('for a living'));
+/////////////////////////////////////////////////
+class Peeps {
+    static getCount() {
+        return Peeps.count;
+    }
+    constructor(name) {
+        this.name = name;
+        this.name = name;
+        this.id = ++Peeps.count;
+    }
+}
+Peeps.count = 0;
+const John = new Peeps('John');
+const Jane = new Peeps('Jane');
+const Jack = new Peeps('Jack');
+console.log(Jack.id);
+console.log(Jane.id);
+console.log(John.id);
+console.log(Peeps.count);
--- vanilla-typescript/src/class.ts
+++ vanilla-typescript/src/class.ts
@@ -1,32 +1,105 @@
-class Point{
-  x= 0;
-  y=0;
-}
+// class Point{
+//   x : number;
+//   y : number;
 
-const point = new Point();
-
-console.log(point.x);
-
-// class Coder {
-//   // name: string;
-//   // music: string;
-//   // age: number;
-//   // lang: string;
-  
-//   constructor(public readonly name: string, public music: string, private age: number, protected lang: string = 'Typescript') {
-//     this.name = name;
-//     this.music = music;
-//     this.lang = lang;
-//     this.age = 25;
-//   }
-
-
-//   public getAge(){
-//     return `Hello, I'm${this.age}`
+//   constructor(x: number, y:number){
+//     this.x = x;
+//     this.y = y;
 //   }
 // }
 
+// const point = new Point(2,3);
 
-// const Dave = new Coder('Dave', 'Rock', 23);
-// console.log(Dave.getAge());
-// // console.log(Dave.age);// console.log(Dave.lang);
+// point.x = 0;
+// point.y = 0;
+// console.log(point);
+
+class Coder {
+  // name: string;
+  // music: string;
+  // age: number;
+  // lang: string;
+  
+  constructor(public readonly name: string, public music: string, private age: number, protected lang: string = 'Typescript') {
+    this.name = name;
+    this.music = music;
+    this.lang = lang;
+    this.age = 25;
+  }
+
+  public getAge(){
+    return `Hello, I'm ${this.age}`
+  }
+}
+
+
+const Dave = new Coder('Dave', 'Rock', 23);
+console.log(Dave.getAge());
+// console.log(Dave.age);
+// console.log(Dave.lang)
+
+
+class WebDev extends Coder{
+  constructor( public computer: string, name: string, music: string, age: number){
+    super(name, music, age);
+    this.computer = computer;
+  }
+
+  public getLang(){
+    return `I write ${this.lang}`
+  }
+}
+
+const Sara = new WebDev('Mac', 'Sara', 'Pop', 23);
+
+console.log(Sara.getLang());
+// console.log(Sara.age);
+// console.log(Sara.lang);
+
+/////////////////////////////////////////////////////////////////////
+interface Musician{
+  name: string,
+  instrument: string,
+  play(action: string): string
+}
+
+class Pianist implements Musician{
+  name: string;
+  instrument: string;
+
+  constructor(name: string, instrument: string){
+    this.name = name;
+    this.instrument = instrument;
+  }
+
+  play(action: string): string {
+    return `${this.name} plays ${this.instrument} ${action}`
+  }
+}
+
+const Orlando = new Pianist('Orlando Owo', 'Piano');
+console.log(Orlando.play('for a living'));
+
+/////////////////////////////////////////////////
+class Peeps{
+  static count: number = 0;
+  static getCount(): number {
+    return Peeps.count
+  }
+
+  public id: number;
+
+  constructor(public name: string){
+    this.name = name;
+    this.id = ++Peeps.count
+  }
+}
+
+const John = new Peeps('John');
+const Jane = new Peeps('Jane');
+const Jack = new Peeps('Jack');
+
+console.log(Jack.id);
+console.log(Jane.id);
+console.log(John.id);
+console.log(Peeps.count);
