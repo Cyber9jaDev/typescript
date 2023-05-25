e775019aba0aa602a935348a32d0eb9d57a424ae
Cyber9ja <ayodejioladapo96@gmail.com>
2023-05-25 21:55:52 +0100

    minor update

--- vanilla-typescript/build/js/generics.js
+++ vanilla-typescript/build/js/generics.js
@@ -15,13 +15,28 @@ const isTrue = (arg) => {
     }
     // Object
     //  Ensure that 'arg' is not a key if 'arg' is an object
-    // if(isObject(arg) && !Object.keys(arg as keyof T).length){
-    //   return { arg, is: false }
-    // }
-    if (isObject(arg)) {
-        Object.keys(arg).forEach(key => console.log(key));
+    if (isObject(arg) && !Object.keys(arg).length) {
+        return { arg, is: false };
     }
     // turn 'is' into a true or false value instead of 0 and 1 by flipping
     return { arg, is: !!arg };
 };
 isTrue({ name: 'John' });
+const checkBoolValue = (arg) => {
+    // Array
+    if (Array.isArray(arg) && !arg.length) {
+        return { value: arg, is: false };
+    }
+    // Object
+    //  Ensure that 'arg' is not a key if 'arg' is an object
+    if (isObject(arg) && !Object.keys(arg).length) {
+        return { value: arg, is: false };
+    }
+    // turn 'is' into a true or false value instead of 0 and 1 by flipping
+    return { value: arg, is: !!arg };
+};
+const processUser = (user) => {
+    return user;
+};
+console.log(processUser({ id: 1, name: 'Dave' }));
+console.log(processUser({ id: 2, f: 'f', name: 'Dave' }));
--- vanilla-typescript/src/generics.ts
+++ vanilla-typescript/src/generics.ts
@@ -18,15 +18,48 @@ const isTrue = <T>(arg: T): { arg: T, is: boolean } => {
   }
   // Object
   //  Ensure that 'arg' is not a key if 'arg' is an object
-  // if(isObject(arg) && !Object.keys(arg as keyof T).length){
-  //   return { arg, is: false }
-  // }
-
-  if(isObject(arg)){
-    Object.keys(arg as keyof T).forEach(key => console.log(key))
+  if(isObject(arg) && !Object.keys(arg as keyof T).length){
+    return { arg, is: false }
   }
+
   // turn 'is' into a true or false value instead of 0 and 1 by flipping
   return { arg, is: !!arg }
 }
 
 isTrue({ name: 'John' });
+
+
+///////////////////////////////////////////////////////////////////////
+
+interface BoolCheck<T>{
+  value: T,
+  is: boolean
+}
+
+const checkBoolValue = <T>(arg: T): BoolCheck<T> => {
+  // Array
+  if(Array.isArray(arg) && !arg.length){
+    return { value: arg, is: false }
+  }
+  // Object
+  //  Ensure that 'arg' is not a key if 'arg' is an object
+  if(isObject(arg) && !Object.keys(arg as keyof T).length){
+    return { value: arg, is: false }
+  }
+
+  // turn 'is' into a true or false value instead of 0 and 1 by flipping
+  return { value: arg, is: !!arg }
+}
+
+///////////////////////////////////////////////////////////////////////
+interface HasID{
+  id: number
+}
+
+const processUser = <T extends HasID>(user: T): T => {
+  return user;
+}
+
+
+console.log(processUser({id: 1, name: 'Dave'}));
+console.log(processUser({id: 2, f: 'f', name: 'Dave'}));
