a2bf21f87732fc7b39ea94e5e572b1e6aa9b729d
Cyber9ja <ayodejioladapo96@gmail.com>
2023-05-27 19:18:10 +0100

    updated utility types

--- vanilla-typescript/build/js/utility_types.js
+++ vanilla-typescript/build/js/utility_types.js
@@ -1,1 +1,47 @@
 "use strict";
+// *******************PARTIAL Utility Type **********************//
+const updateAssignment = (assignment, propsToUpdate) => {
+    return Object.assign(Object.assign({}, assignment), propsToUpdate);
+};
+const studentGrade = {
+    studentId: '12323232323',
+    title: 'Mr Martin',
+    grade: 32323,
+    // verified: false
+};
+console.log(updateAssignment(studentGrade, { grade: 12 }));
+const assignGraded = updateAssignment(studentGrade, { grade: 5 });
+// ******************* Required Utility Type ********************//
+const recordAssignment = (assignment) => {
+    // Send to database e.t.c
+    return assignment;
+};
+// ******************* Readonly Utility Type ********************//
+const assignVerified = Object.assign(Object.assign({}, assignGraded), { verified: true });
+// assignVerified.grade = 4; // Error, assignVerified is read only
+// recordAssignment(assignGraded);  // Error, assignGraded has a 'verified' prop as an optional value
+recordAssignment(Object.assign(Object.assign({}, assignGraded), { verified: true }));
+// ******************* Record Utility Type ********************//
+const hexColorMap = {
+    red: '#ff0000',
+    green: '#00ff00',
+    blue: '#0000ff',
+};
+const finalGrades = {
+    Sara: 'B',
+    Kelly: 'C'
+};
+const gradeData = {
+    Sara: { assign1: 65, assign2: 0 },
+    Kelly: { assign1: 0, assign2: 434 }
+};
+const score = {
+    studentId: 'k123',
+    grade: 86
+};
+console.log(score);
+const preview = {
+    studentId: 'k123',
+    title: 'Final Project',
+};
+console.log(preview);
---
+++ vanilla-typescript/src/utility_types.ts
@@ -0,0 +1,96 @@
+// *******************PARTIAL Utility Type **********************//
+
+interface Assignment{
+  studentId: string,
+  title: string,
+  grade: number,
+  verified?: boolean
+}
+
+const updateAssignment = (assignment: Assignment, propsToUpdate: Partial<Assignment>): Assignment => {
+  return {
+    ...assignment, ...propsToUpdate
+  }
+}
+
+const studentGrade: Assignment = {
+  studentId: '12323232323',
+  title: 'Mr Martin',
+  grade: 32323,
+  // verified: false
+}
+
+console.log(updateAssignment( studentGrade, { grade: 12 } ));
+
+const assignGraded: Assignment = updateAssignment(studentGrade, { grade: 5 });
+
+// ******************* Required Utility Type ********************//
+
+const recordAssignment = (assignment: Required<Assignment>): Assignment => {
+  // Send to database e.t.c
+  return assignment;
+}
+
+// ******************* Readonly Utility Type ********************//
+
+const assignVerified: Readonly<Assignment> = {
+  ...assignGraded, verified: true
+}
+
+// assignVerified.grade = 4; // Error, assignVerified is read only
+// recordAssignment(assignGraded);  // Error, assignGraded has a 'verified' prop as an optional value
+recordAssignment({...assignGraded, verified: true});
+
+
+// ******************* Record Utility Type ********************//
+const hexColorMap: Record<string, string>  = {
+  red: '#ff0000',
+  green: '#00ff00',
+  blue: '#0000ff',
+}
+
+type Students = 'Sara' | 'Kelly';
+type LetterGrades = 'A' | 'B' | 'C' | 'D' | 'U';
+
+const finalGrades: Record<Students, LetterGrades> = {
+  Sara: 'B',
+  Kelly: 'C'
+}
+
+interface Grades{
+  assign1: number,
+  assign2: number
+}
+
+const gradeData: Record<Students, Grades> = {
+  Sara: { assign1: 65, assign2: 0 },
+  Kelly: { assign1: 0, assign2: 434 }
+}
+
+// ******************* Pick Utility Type ********************//
+type AssignResult = Pick<Assignment, 'studentId' | 'grade'> 
+
+const score: AssignResult = {
+  studentId: 'k123',
+  grade: 86
+}
+console.log(score);
+
+// ******************* Omit Utility Type ********************//
+type AssignPreview = Omit<Assignment, 'grade' | 'verified'>
+
+const preview: AssignPreview = {
+  studentId: 'k123',
+  title: 'Final Project',
+}
+
+console.log(preview);
+
+// ******************* Exclude and Extract Utility Type ********************//
+type adjustedGrade = Exclude<LetterGrades, 'U'>
+
+type highGrades = Extract<LetterGrades, 'A' | 'B'>
+
+
+// ******************* NonNullable  Utility Type ********************//
+type AllPossibleGrades = 'Dave' | 'John' | null | undefined
