f86f80718b1cc842e0e53a5b87a5cbc629efa799
Cyber9ja <ayodejioladapo96@gmail.com>
2023-05-28 23:59:20 +0100

    update

--- vite-ts-project/@latest/src/model/FullList.ts
+++ vite-ts-project/@latest/src/model/FullList.ts
@@ -10,31 +10,44 @@ interface List{
 } 
 
 export default class FullList implements List{
-  // Only one instance of FullList is required
+  // Only one instance of FullList is required, we need to refer to this class
   static instance: FullList = new FullList();
 
-  private constructor(
-    private _list: ListItem[]= []
-  ){}
+  // There will only be one instance of this class created
+  private constructor( private _list: ListItem[]= []){}
+
+  get list():  ListItem[] {
+    return this._list
+  }
 
   load(): void {
+    const storedList: string | null = localStorage.getItem('list');
+    if(typeof storedList !== 'string') return
 
+    const parsedList: { _id: string, _item: string, checked: boolean }[] = JSON.parse(storedList);
+    
+    parsedList.forEach(itemObj => {
+      const newListItem = new ListItem(itemObj._id, itemObj._item, itemObj.checked);
+    });
   }
 
   save(): void {
-    
+    localStorage.setItem('list', JSON.stringify(this._list));
   }
 
   clearList(): void {
     this._list = [];
+    this.save();
   }
 
   addItem(itemObj: ListItem ): void {
     this._list.push(itemObj);
+    this.save();
   }
 
   removeItem(id: string): void {
-    
+    this._list = this._list.filter(item => item.id !== id);
+    this.save();
   }
 
 }
