8406ce5385b7b7f77b13a2a0b326b0c102585d7b
Cyber9ja <ayodejioladapo96@gmail.com>
2023-05-29 09:41:31 +0100

    minor update

--- vite-ts-project/@latest/src/main.ts
+++ vite-ts-project/@latest/src/main.ts
@@ -1,2 +1,39 @@
 import './css/style.css';
+import FullList from './model/FullList';
+import ListItem from './model/ListItem';
+import ListTemplate from './templates/ListTemplate';
 
+
+const initApp = (): void => {
+  const fullList  = FullList.instance;  //Singleton instance of FullList 
+  const template = ListTemplate.instance;
+
+  const itemEntryForm = document.getElementById('itemEntryForm') as HTMLFormElement;
+  itemEntryForm.addEventListener('submit', (event: SubmitEvent): void => {
+    event.preventDefault();
+
+    const input = document.getElementById('clearItemsButton') as HTMLButtonElement;
+    const newEntryText: string = input.value.trim();
+    if(!newEntryText) return;
+
+    const itemId: number = fullList.list.length ? parseInt(fullList.list[fullList.list.length - 1].id) + 1 : 1;
+
+    const newItem = new ListItem(itemId.toString(), newEntryText);
+
+    fullList.addItem(newItem);
+
+    template.render(fullList); 
+
+  });
+
+  const clearItemsButton = document.getElementById("clearItemsButton") as HTMLButtonElement;
+  clearItemsButton.addEventListener("click", (): void => {
+    fullList.clearList();
+    template.clear();
+  }); 
+
+  fullList.load();
+  template.render(fullList);
+}
+
+document.addEventListener('DOMContentLoaded', initApp);
