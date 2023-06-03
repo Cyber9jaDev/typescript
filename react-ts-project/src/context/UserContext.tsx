e525db7bbc941f4e5f9e1e13305bf275e4c44f68
Cyber9ja <ayodejioladapo96@gmail.com>
2023-06-03 23:46:06 +0100

    update

---
+++ react-ts-project/src/context/UserContext.tsx
@@ -0,0 +1,27 @@
+import React, { createContext, ReactNode, useState } from 'react';
+
+export type AuthUser = {
+  username: string,
+  email: string,
+}
+
+type UserContextType = {
+  user: AuthUser | null,
+  setUser: React.Dispatch<React.SetStateAction<AuthUser | null>>
+}
+
+type ChildrenType = {
+  children: ReactNode,
+}
+
+export const UserContext = createContext<UserContextType | null>(null);
+
+export const UserContextProvider = ({ children }: ChildrenType) => {
+  const [user, setUser] = useState<AuthUser | null>(null);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  )
+}
