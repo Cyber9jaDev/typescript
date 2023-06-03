eb60bb68e200155115435496a7de1efe631bd371
Cyber9ja <ayodejioladapo96@gmail.com>
2023-06-03 23:57:29 +0100

    update

--- react-ts-project/src/App.tsx
+++ react-ts-project/src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import TieFighter from './components/TieFighter'
 import { AwesomeContextProvider } from './context/AwesomeContext';
+import { UserContextProvider } from './context/UserContext';
+import User from './components/User';
 
 function App() {
   return (
-    <AwesomeContextProvider>
-      <h2>React Context</h2>
-      <TieFighter />
-    </AwesomeContextProvider>
+    <>
+      <AwesomeContextProvider>
+        <h2>React Context</h2>
+        <TieFighter />
+      </AwesomeContextProvider>
+      <UserContextProvider>
+        <User />
+      </UserContextProvider>
+    
+    </>
   )
 }
 
---
+++ react-ts-project/src/components/User.tsx
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import React, { useContext } from 'react';
+import { UserContext } from '../context/UserContext';
+
+
+const User = () => {
+  const userContext = useContext(UserContext);
+
+  const handleLogin = () => { 
+    if(userContext){
+      userContext.setUser({
+        username: 'XXXX',
+        email: 'yes@gmail.com'
+      })
+    }
+  }
+  const handleLogout = () => {
+    if(userContext){
+      userContext.setUser(null)
+    }
+  }
+
+
+  return (
+    <div>
+      <button onClick={handleLogin}>Login</button>
+      <button onClick={handleLogout}>Logout</button>
+      <span> User name is {userContext?.user?.username} </span>
+      <span> User email is {userContext?.user?.email}</span>
+    </div>
+  )
+}
+
+export default User
