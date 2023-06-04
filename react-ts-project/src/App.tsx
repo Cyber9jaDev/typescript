a90cd91c9691b23cf537229a44906527f07ab81d
Cyber9ja <ayodejioladapo96@gmail.com>
2023-06-04 20:59:25 +0100

    updated cart

--- react-ts-project/src/App.tsx
+++ react-ts-project/src/App.tsx
@@ -4,18 +4,21 @@ import ProductList from './components/ProductList';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { CartProvider } from './context/CartProvider';
+import { ProductsProvider } from './context/ProductsProvider';
 
 function App() {
   const [viewCart, setViewCart] = useState<boolean>(false);
   
   const pageContent = viewCart ? <Cart /> : <ProductList />;
-
+  
   const content = (
-    <CartProvider>
-      <Header viewCart={viewCart} setViewCart={setViewCart}  />
-      { pageContent }
-      <Footer viewCart={viewCart} />
+    <ProductsProvider>
+      <CartProvider>
+        <Header viewCart={viewCart} setViewCart={setViewCart} />
+        { pageContent }
+        <Footer viewCart={viewCart} />
     </CartProvider>
+    </ProductsProvider>
   );
 
   return content;
