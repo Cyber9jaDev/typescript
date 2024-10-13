41c2427064c74ba0b107ae88e11034d879b7fcf5
cyber9ja <ayodejioladapo15@gmail.com>
2024-10-13 20:06:28 +0100

    minor fix

--- oop/src/main.ts
+++ oop/src/main.ts
@@ -1,11 +1,79 @@
-function welcomePerson(person: { firstName: string, lastName: string }) {
-  console.log(`Hey ${person.firstName} ${person.lastName}`);
-  return `Hey ${person.firstName} ${person.lastName}`;
-}
+// ******************* Encapsulation*************************
 
-const james = {
-  firstName: "James",
-  lastName: "Quick"
-};
 
-welcomePerson(james);
+
+
+
+
+
+
+
+// ******************* Encapsulation*************************
+// class Player {
+//   private speed: number;
+//   private health: number;
+
+//   setHealth(health: number) {
+//     if (health < 0){
+//       console.log('Forbidden!');
+//     };
+
+//     this.health = health;
+//   }
+
+//   getHealth(): number {
+//     return this.health;
+//   }
+
+//   setSpeed(speed: number) {
+//     this.speed = speed;
+//   }
+
+//   getSpeed(): number {
+//     return this.speed;
+//   }
+  
+// }
+
+// const mario = new Player();
+// mario.setHealth(-5);
+// mario.setHealth(7);
+// mario.setSpeed(1);
+
+// console.log(`Mario has ${mario.getHealth()}/10 health`);
+// console.log(`Mario has speed of ${mario.getSpeed()}`);
+
+
+
+// ************************ 1 *******************************
+// class Player {
+//   constructor(
+//     public name: string,
+//     public health: number,
+//     public isInvincible : boolean,
+//     public crush?: Player,
+//   ){}
+
+//   greet(){
+//     console.log(`Hello ${this.name}`);
+//   }
+  
+// }
+
+// const mario = new Player('Mario', 3, true);
+// // mario.name = 'Mario'
+// // mario.health = 100
+// // mario.isInvincible = false
+
+// const peach = new Player('Peach', 43, false);
+// // peach.name = 'Peach'
+// // peach.health = 0
+// // peach.isInvincible = true
+
+// // 
+// // mario.crush = peach
+// // console.log(mario.crush.name);
+// mario.greet();
+// peach.greet();
+
+
--- vanilla-ts/src/class.ts
+++ vanilla-ts/src/class.ts
@@ -19,86 +19,94 @@ class Coder {
   // music: string;
   // age: number;
   // lang: string;
-  
-  constructor(public readonly name: string, public music: string, private age: number, protected lang: string = 'Typescript') {
+
+  constructor(
+    public readonly name: string,
+    public music: string,
+    private age: number,
+    protected lang: string = "Typescript"
+  ) {
     this.name = name;
     this.music = music;
     this.lang = lang;
     this.age = 25;
   }
 
-  public getAge(){
-    return `Hello, I'm ${this.age}`
+  public getAge() {
+    return `Hello, I'm ${this.age}`;
   }
 }
 
-
-const Dave = new Coder('Dave', 'Rock', 23);
+const Dave = new Coder("Dave", "Rock", 23);
 console.log(Dave.getAge());
 // console.log(Dave.age);
 // console.log(Dave.lang)
 
-
-class WebDev extends Coder{
-  constructor( public computer: string, name: string, music: string, age: number){
+class WebDev extends Coder {
+  constructor(
+    public computer: string,
+    name: string,
+    music: string,
+    age: number
+  ) {
     super(name, music, age);
     this.computer = computer;
   }
 
-  public getLang(){
-    return `I write ${this.lang}`
+  public getLang() {
+    return `I write ${this.lang}`;
   }
 }
 
-const Sara = new WebDev('Mac', 'Sara', 'Pop', 23);
+const Sara = new WebDev("Mac", "Sara", "Pop", 23);
 
 console.log(Sara.getLang());
 // console.log(Sara.age);
 // console.log(Sara.lang);
 
 /////////////////////////////////////////////////////////////////////
-interface Musician{
-  name: string,
-  instrument: string,
-  play(action: string): string
+interface Musician {
+  name: string;
+  instrument: string;
+  play(action: string): string;
 }
 
-class Pianist implements Musician{
+class Pianist implements Musician {
   name: string;
   instrument: string;
 
-  constructor(name: string, instrument: string){
+  constructor(name: string, instrument: string) {
     this.name = name;
     this.instrument = instrument;
   }
 
   play(action: string): string {
-    return `${this.name} plays ${this.instrument} ${action}`
+    return `${this.name} plays ${this.instrument} ${action}`;
   }
 }
 
-const Orlando = new Pianist('Orlando Owo', 'Piano');
-console.log(Orlando.play('for a living'));
+const Orlando = new Pianist("Orlando Owo", "Piano");
+console.log(Orlando.play("for a living"));
 
 /////////////////////////////////////////////////
-class Peeps{
+class Peeps {
   static count: number = 0;
   static getCount(): number {
-    return Peeps.count
+    return Peeps.count;
   }
 
   public id: number;
 
-  constructor(public name: string){
+  constructor(public name: string) {
     this.name = name;
-    this.id = ++Peeps.count
+    this.id = ++Peeps.count;
   }
 }
 
-const John = new Peeps('John');
-const Jane = new Peeps('Jane');
-const Jack = new Peeps('Jack');
-const Jackal = new Peeps('Jackal');
+const John = new Peeps("John");
+const Jane = new Peeps("Jane");
+const Jack = new Peeps("Jack");
+const Jackal = new Peeps("Jackal");
 
 console.log(Jack.id);
 console.log(Jane.id);
@@ -107,28 +115,30 @@ console.log(Peeps.count);
 console.log(Peeps.getCount());
 
 //////////////////////////////////////////////////////////////
-class Bands{
+class Bands {
   private dataState: string[];
 
-  constructor(){
+  constructor() {
     this.dataState = [];
   }
 
-  public get data() : string[] {
+  public get data(): string[] {
     return this.dataState;
   }
 
-  public set data(value : string[]) {
-    if(Array.isArray(value) && value.every(element => typeof element === 'string')){
+  public set data(value: string[]) {
+    if (
+      Array.isArray(value) &&
+      value.every((element) => typeof element === "string")
+    ) {
       this.dataState = value;
       return;
-    } else throw new Error('Param is not an array of strings')
+    } else throw new Error("Param is not an array of strings");
   }
 }
 
 const myBands = new Bands();
-myBands.data = ['Pink Floyd', 'Queen', 'Metallica'];
+myBands.data = ["Pink Floyd", "Queen", "Metallica"];
 console.log(myBands.data);
-
 
 /////////////////////////////////////////////////////////////
